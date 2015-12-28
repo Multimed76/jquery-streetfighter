f084e7156965e71a2e0dd1d35fbaa924fd5baa05
Multimed76 <flashdev76@gmail.com>
2015-12-27 19:10:55 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
     $('.ryu').mouseenter(function(){
         $('.ryu-still').hide();
         $('.ryu-ready').show();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide();
     })
     .mouseleave(function() {
         $('.ryu-still').show();
         $('.ryu-ready').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide();
     })
     .mousedown(function(){
         playHadouken();
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
         $('.hadouken').finish().show().animate(
@@ -21,9 +27,27 @@ $(document).ready(function(){
     })
     .mouseup(function(){
         $('.ryu-throwing').hide();
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
         $('.ryu-ready').show();
     });
 
+})
+$(document).keydown(function() {
+	if (event.which == 88) {
+		$('.ryu-cool').show();
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').hide();
+	}
+})
+	$(document).keyup(function() {
+		if (event.which == 88) {
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').hide();
+	}
 });
 
 function playHadouken() {
